961f0502e20ee40a23b7778b27793b8f4cc90a6e
taptap <alexey.e.egorov@gmail.com>
2016-12-29 19:09:16 +0300

    add color implementation
    As 32-bit integer, and also CGI conformant color list with names and
    corresponding values.

--- graphics.lisp
+++ graphics.lisp
@@ -1,10 +1,50 @@
 (in-package :graphics)
 
-(defvar *color* 'black
-  "Current outline color")
+(defmacro color-rgba (r g b a)
+  (logior (ash r 24)
+          (ash g 16)
+          (ash b 8)
+          a))
 
-(defvar *fill-color* 'black
-  "Current outline color")
+(defun r (int-32)
+  (ash int-32 -24))
+
+(defun g (int-32)
+  (ash
+   (logand int-32 (ash 255 16))
+   -16))
+
+(defun b (int-32)
+  (ash
+   (logand int-32 (ash 255 8))
+   -8))
+
+(defun a (int-32)
+  (logand int-32 255))
+
+(defparameter +color-list+
+  (list ('black . (color-rgba 0 0 0 0))
+        ('blue . (color-rgba 0 0 170 0))
+        ('green . (color-rgba 0 170 0 0))
+        ('cyan . (color-rgba 0 170 170 0))
+        ('red . (color-rgba 170 0 0 0))
+        ('magenta . (color-rgba 170 0 170 0))
+        ('brown . (color-rgba 170 85 0 0))
+        ('lightgray . (color-rgba 170 170 170 0))
+        ('darkgray . (color-rgba 85 85 85 0))
+        ('lightblue . (color-rgba 85 85 255 0))
+        ('lightgreen . (color-rgba 85 255 85 0))
+        ('lightcyan . (color-rgba 85 255 255 0))
+        ('lightred . (color-rgba 255 85 85 0))
+        ('lightmagenta . (color-rgba 255 85 255 0))
+        ('yellow . (color-rgba 255 255 85 0))
+        ('white . (color-rgba 255 255 255 0))))
+
+(defun color (num-or-name)
+  "Gets color from list by name or position"
+  (cdr (if (symbolp num-or-name)
+           (assoc num-or-name +color-list+)
+           (nth num-or-name +color-list+))))
 
 (defvar *surface* nil
   "Current drawing surface")
